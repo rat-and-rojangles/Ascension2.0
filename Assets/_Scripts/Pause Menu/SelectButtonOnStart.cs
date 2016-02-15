30f00a94f2f6bbfd0831ffd31355bc41c91cb5f7
Brian Fischer <starfox139@yahoo.com>
2016-02-15 17:52:17 -0500

    pause menu shaping up

---
+++ Assets/_Scripts/Pause Menu/SelectButtonOnStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+//the button with this script attached will be selected by default
+public class SelectButtonOnStart : MonoBehaviour {
+
+	void OnEnable () {
+		GetComponent<Button> ().Select ();
+	}
+
+}
